e70dcdf27a304ab6b2957d09ff1b0a57fc6bb9f0
Esin <nbusra.esin@gmail.com>
2023-07-02 18:04:15 +0300

    feature:add dark mode css

--- app/(Dashboard)/learning/page.jsx
+++ app/(Dashboard)/learning/page.jsx
@@ -1,24 +1,54 @@
+"use client"
 import React from 'react'
+import Link from 'next/link'
+import { useContext } from 'react'
+
+import { DarkModeContext } from '../layout'
 
 import CourseDescription from '@/app/components/learning/CourseDescription'
 
 const page = () => {
+  const mode = useContext(DarkModeContext)
+  const isDark = !mode
+  //bg-[#253762]
+  //bg-[#0F172A]
+
   const style ={
-    main: `flex flex-col sm:flex-row bg-gray-100`,
+    main: `flex flex-col sm:flex-row font-bold rounded-lg`,
     left_section_light : `  w-full sm:w-screen-1/2 p-5`,
-    right_section_light: `bg-white w-full sm:w-screen-1/2 h-max`,
-    h1_light: `text-3xl text-gray-800 font-bold`,
+    right_section: ` w-full sm:w-screen-1/2 h-max p-5 `,
+    buttonContainer:`w-full text-sm grid grid-cols-1 md:grid-cols-2 gap-4 `,
+    button: `w-full border font-bold py-2 px-4 rounded-lg hover:translate-y-1 transition-all`,
+    linkButton:` w-full font-bold py-2 px-4 rounded-lg hover:translate-y-1 transition-all`,
   }
+
+  const light = {
+    main:`bg-gray-100 text-stone-800`,
+    right_section: `bg-white `,
+    button: `text-[#0F172A] hover:text-white bg-white-500 border-[#0F172A] hover:bg-[#253762] `,
+    linkButton:`bg-[#2d4781] hover:bg-[#253762] text-white  `,
+  }
+
+  const dark ={
+    main: `bg-gray-700 text-stone-200`,
+    right_section: `bg-[#253762] rounded-lg`,
+    button: `text-stone-200 hover:text-[#0F172A] bg-[#253762] border-stone-200 hover:bg-stone-300`,
+    linkButton:`bg-[#0F172A] hover:bg-[#3c5fad] text-white  `,
+
+  }
+
   return (
-    <div className={style.main}>
+    <div className={`${style.main} ${isDark ? dark.main : light.main}`}>
       <div className={style.left_section_light}>
-          <h1 className={style.h1_light}>My Learning</h1>
+          <h1>My Learning</h1>
       </div>
-      <div className={style.right_section_light}>
-        <CourseDescription />
-        <div className='flex flex-row space-x-3 p-3 text-sm'>
-          <button className='text-blue-500 hover:text-white bg-white-500 border border-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg w-full'>REVIEW COURSE</button>
-          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg w-full'>CONTINUE LEARNING</button>
+      <div className={`${style.right_section} ${isDark? dark.right_section : light.right_section}`}>
+        <CourseDescription isDark={isDark}/>
+        <div className={style.buttonContainer}>
+          <button className={`${style.button} ${isDark? dark.button : light.button}`}>REVIEW COURSE</button>
+          <Link href="/#">
+            <button className={`${style.linkButton} ${isDark? dark.linkButton : light.linkButton}`}>CONTINUE LEARNING</button>
+          </Link>
         </div>
       </div>
     </div>
--- app/components/learning/CourseDescription.jsx
+++ app/components/learning/CourseDescription.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import {AiFillClockCircle, AiFillStar} from "react-icons/ai"
 
-const CourseDescription = () => {
-  const light_style = {
-    container : ` p-5 rounded-xl text-gray-700 h-full flex flex-col `,
+const CourseDescription = ({isDark}) => {
+
+  console.log(isDark)
+  const style = {
+    container : ` p-5 rounded-xl  h-full flex flex-col `,
     image_container : `bg-cover bg-center bg-no-repeat rounded-xl border-0 `,
-    course_title : ` text-xl font-bold`
-
+    course_title : ` text-xl font-bold`,
+    gray_500 : `text-gray-500`
   }
+
+  const dark = {
+    course_title : `text-stone-200`,
+  }
+
+  //PROPS - MAY COME FROM REDUX LIB WHEN THE API IS DETERMINED AND LIBARY IS CONSTRUCTED
   const imgUrl = "https://images.unsplash.com/photo-1541516160071-4bb0c5af65ba?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHwxfHxEYXNoYWhlJTIwUGFyayUyQyUyMFNoZW56aGVuJTIwU2hpJTJDJTIwQ2hpbmF8ZW58MXx8fHwxNjUxNzYxNjEw&ixlib=rb-1.2.1&q=80&w=1080"
 
   const courseTitle = "Becoming a Photographer"
@@ -20,11 +29,11 @@ const CourseDescription = () => {
 
   return (
 
-    <div className={light_style.container}>
-        <img className={light_style.image_container} src={imgUrl} alt="" />
+    <div className={`${style.container} ${isDark? dark.course_title : ""}`}>
+        <img className={style.image_container} src={imgUrl} alt="" />
       
       <div className='p-4 space-y-5'>
-        <h2 className={light_style.course_title}>{courseTitle}</h2>
+        <h2 className={style.course_title}>{courseTitle}</h2>
         
         <div className='flex flex-row items-between items-center w-max rounded-full shadow p-1' >
         <img
@@ -32,17 +41,24 @@ const CourseDescription = () => {
           alt="trainer"
           className="object-cover rounded-full shadow w-9 h-9"
        />
-        <p className='px-2 ' >Clara Manning</p>
+        <p className='px-2 '> Clara Manning</p>
       </div>
 
-      <div>  
-      <p className='text-gray-500'>{time}m</p>
-      <p className='text-gray-500'>{rating}/5.0</p>
+      <div> 
+      <div className='flex items-center space-x-1 text-gray-500'>
+        <AiFillClockCircle />
+        <p>{time}m</p>
+      </div>  
+
+      <div className='flex items-center space-x-1 text-gray-500'>
+        <AiFillStar />
+        <p>{rating}/5.0</p>
+      </div> 
       </div>
 
       <div className='space-y-3'>
         <h3 className='font-bold text-lg '>Course Description</h3>
-        <p className='text-gray-500'>{courseDescriptionText}</p>
+        <p className={style.gray_500}>{courseDescriptionText}</p>
       </div>
     </div>
 
