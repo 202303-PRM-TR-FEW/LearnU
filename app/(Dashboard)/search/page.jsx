afd50540e136baf63e5a9b6598b21587bae4d5a7
Bilal-hajM <bblal1003@gmail.com>
2023-07-05 22:28:39 +0300

    feat: add top search categories and rating section
    * Added a top search categories section to the home page.
    * The section lists the most popular search categories,
    * Added a rating section to the product pages.
    * The section allows a sellection for the average rating of a product,
    * The changes were tested with unit tests.
    
    Signed-off-by: Bill

--- app/(Dashboard)/search/page.jsx
+++ app/(Dashboard)/search/page.jsx
@@ -3,7 +3,7 @@ import SearchForm from "@/app/components/search/seachForm";
 
 function page () {
     const styling = {
-        container: "ml-20",
+        container: "ml-10 sm:ml-20",
         textHeader: "text-4xl flex-grow-1 m-0 overflow-hidden text-32 font-medium text-left tracking-normal leading-42 whitespace-pre opacity-100 visible font-rubik"
     }
     return (
