2444a84ff02c4e2cc3a3e914971f27580f401e34
Esin <nbusra.esin@gmail.com>
2023-07-05 20:16:59 +0300

    feature: fetch the db data and display the data on learning page. connected to components

--- app/components/learning/CourseCard.jsx
+++ app/components/learning/CourseCard.jsx
@@ -1,18 +1,28 @@
+"use client"
 import React from 'react'
+import {createContext, useState} from "react"
 
-const CourseCard = ( {isDark, title, trainer} ) => {
-    const imgUrl = "https://images.unsplash.com/photo-1541516160071-4bb0c5af65ba?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHwxfHxEYXNoYWhlJTIwUGFyayUyQyUyMFNoZW56aGVuJTIwU2hpJTJDJTIwQ2hpbmF8ZW58MXx8fHwxNjUxNzYxNjEw&ixlib=rb-1.2.1&q=80&w=1080"
+export const CourseIdContext = createContext();
+
+const CourseCard = ( {isDark, title, trainer, imgUrl, courseId, setCourseId } ) => {
+    
+    const handleClick = () => {
+        setCourseId(courseId);
+      };
 
     //PROPS START
     const percentage = "45"
     //PROPS END
     
+    // const [courseId, setCourseId] = useState("");
+
+
     const dark = {
-        bg_dark: `bg-[#20293a] hover:bg-gray-600 hover:border-gray-500`,
+        bg_dark: `bg-gray-500 hover:bg-gray-600 hover:border-gray-500 text-stone-200`,
     }
 
   return (
-    <div className={`group shadow-sm text-xs sm:text-md font-normal w-full grid grid-cols-4  bg-white rounded-2xl  p-2 hover:border border-blue-300 hover:bg-blue-100 ${isDark? dark.bg_dark : ""} `}>
+    <div className={` group shadow-sm text-xs sm:text-md font-normal w-full grid grid-cols-4  bg-white rounded-2xl  p-2 hover:border border-blue-300 hover:bg-blue-100 ${isDark? dark.bg_dark : ""} `} onClick={handleClick}>
 
         <div className='left-section flex flex-grow items-center justify-center '>
             <div className='w-full h-full'>
@@ -23,14 +33,14 @@ const CourseCard = ( {isDark, title, trainer} ) => {
         <div className='right-section col-span-3 space-y-2 sm:space-y-5 p-4'>
             <div>
                 <h1 className='sm:text-lg font-bold'> {title} </h1>
-                <p className='text-stone-500'> {trainer} </p>
+                <p className='text-stone-400'> {trainer} </p>
             </div>
 
             <div className='sm:space-y-2'>
                 <div className=' progress-bar w-full sm:h-2.5 bg-gray-200 rounded-full group-hover:bg-white'>
-                    <div className=" sm:h-2.5 bg-blue-500 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${percentage}%` }}  > </div>
+                    <div className=" sm:h-2.5 bg-blue-500 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${percentage}%` }}> </div>
                 </div>
-                <p className='text-stone-500'> <span> {percentage}% </span> complete </p>
+                <p className='text-stone-400'> <span> {percentage}% </span> complete </p>
             </div>
         </div>
     </div>
--- app/components/learning/CourseDescription.jsx
+++ app/components/learning/CourseDescription.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import {AiFillClockCircle, AiFillStar} from "react-icons/ai"
 
-const CourseDescription = ({isDark}) => {
-
+const CourseDescription = ({isDark, selectedCourse}) => {
+  
   const style = {
     container : ` p-5 rounded-xl  h-full flex flex-col font-normal text-sm `,
     image_container : `bg-cover bg-center bg-no-repeat rounded-xl border-0 `,
@@ -14,44 +14,40 @@ const CourseDescription = ({isDark}) => {
     course_title : `text-stone-200`,
   }
 
-  //PROPS - MAY COME FROM REDUX LIB WHEN THE API IS DETERMINED AND LIBARY IS CONSTRUCTED
-  const imgUrl = "https://images.unsplash.com/photo-1541516160071-4bb0c5af65ba?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHwxfHxEYXNoYWhlJTIwUGFyayUyQyUyMFNoZW56aGVuJTIwU2hpJTJDJTIwQ2hpbmF8ZW58MXx8fHwxNjUxNzYxNjEw&ixlib=rb-1.2.1&q=80&w=1080"
-
-  const courseTitle = "Becoming a Photographer"
-
-  const trainerSvg = "https://avatars.githubusercontent.com/u/78679521?v=4" 
-
-  const time = "43"
-  const rating = "4.7"
+  
   
   const courseDescriptionText = "This comprehensive course is designed to provide a solid foundation in web development. Whether you're a beginner or have some experience, this course will take you through the essentials of building websites from scratch."
+
+  if (!selectedCourse) {
+    return null; // or return a loading state if desired
+  }
 
   return (
 
     <div className={`${style.container} ${isDark? dark.course_title : ""}`}>
-        <img className={style.image_container} src={imgUrl} alt="" />
+        <img className={style.image_container} src={selectedCourse.img} alt="" />
       
       <div className='p-4 space-y-5'>
-        <h2 className={style.course_title}>{courseTitle}</h2>
+        <h2 className={style.course_title}>{selectedCourse.courseName}</h2>
         
         <div className='flex flex-row items-between items-center w-max rounded-full shadow p-1' >
         <img
-          src={trainerSvg}
+          src={selectedCourse.img}
           alt="trainer"
           className="object-cover rounded-full shadow w-9 h-9"
        />
-        <p className='px-2 '> Clara Manning</p>
+        <p className='px-2 '> {selectedCourse.trainerName}</p>
       </div>
 
       <div> 
       <div className='flex items-center space-x-1 text-gray-500'>
         <AiFillClockCircle />
-        <p>{time}m</p>
+        <p>{selectedCourse.duration.hours}h {selectedCourse.duration.mins}m</p>
       </div>  
 
       <div className='flex items-center space-x-1 text-gray-500'>
         <AiFillStar />
-        <p>{rating}/5.0</p>
+        <p>{selectedCourse.rating}/5.0</p>
       </div> 
       </div>
 
