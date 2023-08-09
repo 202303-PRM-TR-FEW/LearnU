1bfaa8eb96470317e4844b7ee0e691697c2ddc68
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2023-08-09 14:15:23 +0300

    ✨ feat(return to): add redirect to func
    add return to function in auth file to redirect the user after the login and the logout to the home page instead of the intro page

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,4 +1,7 @@
 // pages/api/auth/[auth0].js
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogout,handleLogin } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+export default handleAuth({
+  logout: handleLogout({ returnTo: 'http://localhost:3000/home' }),
+    login: handleLogin({ returnTo: 'http://localhost:3000/home' }),
+});
