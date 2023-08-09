4241c20a097fcc0d73e025ffaa9485fc388541b5
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2023-08-09 14:12:10 +0300

    🌈 style(button): view more button
    change the style of view more button to be in th center of the page instead of left side

--- app/(Dashboard)/home/page.jsx
+++ app/(Dashboard)/home/page.jsx
@@ -50,7 +50,7 @@ export default function page() {
                 ))}
             </div>
             </Reveal>
-                <button className="self-center px-4 py-2 mt-2 mb-8 text-xl text-center text-white bg-blue-600 rounded-lg disabled:cursor-not-allowed disabled:bg-blue-950"
+                <button className="self-center px-4 py-2 mb-8 text-xl text-center text-white bg-blue-600 rounded-lg disabled:cursor-not-allowed disabled:bg-blue-950"
                 onClick={() => setCoursesViewer(prev => prev+4)} disabled={coursesViewer >= otherCourses.length}>View more</button>
             <Reveal delay="0.7">
             <Title title="Categories" />
