ddcb324cbd1f39177f5813e5a94cf4309e14f29d
Esin <nbusra.esin@gmail.com>
2023-08-10 12:55:42 +0300

    feature: next-intl implementation. only intro page is done

--- app/[locale]/(Dashboard)/layout.jsx
+++ app/[locale]/(Dashboard)/layout.jsx
@@ -1,5 +1,5 @@
 "use client"
-import Aside from "../components/dashboard/Aside";
+import Aside from "../../components/dashboard/Aside";
 import { useState, createContext, useEffect } from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { ToastContainer } from "react-toastify";
